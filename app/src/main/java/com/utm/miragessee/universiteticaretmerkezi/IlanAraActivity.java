bb2d43bbe38f8d8b4a1f2667dfda52d2ced2394a
Orion07 <aligocmen7@gmail.com>
2016-04-06 00:31:26 +0300

    IlanAraActivity eklendii22

--- app/src/main/java/com/utm/miragessee/universiteticaretmerkezi/IlanAraActivity.java
+++ app/src/main/java/com/utm/miragessee/universiteticaretmerkezi/IlanAraActivity.java
@@ -11,7 +11,7 @@ public class IlanAraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ilan_ara);
-        //testtttadsfasdf
+        //testtttadsfasdfasdfasdf
     }
 
     @Override
